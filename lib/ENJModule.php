874511010ce1d8186cee9dcfb5e477e02ac70d53
Grant Robinson <Chisdeal2014@hotmail.com>
2021-06-08 14:40:36 +0100

    Create ENJModule.php

---
+++ lib/ENJModule.php
@@ -0,0 +1,108 @@
+<?php 
+
+namespace ENJModule;
+
+use GuzzleHttp\Client;
+
+class ENJModule{
+    
+    public function __construct()
+    {
+        $this->explorer_url = "https://eth.flitswallet.app/api/";
+        $this->client = new client();
+    }
+
+    //
+    // Look in the explorer if a newer transaction than $timestap in ms (int) with $amount (Float) exists for $address (varchar)
+    //
+    public function existsTransaction($address, $amount, $timestamp)
+    {
+        try{
+            $transactions = $this->getAddressTransactions($address);
+
+            foreach($transactions as $transaction)
+            {
+                $transaction_info = $this->getTransaction($transaction);
+
+                //allowing only unconfirmed transactions & confirmed transactions newer than $timestamp
+                if($transaction_info['blocktime'] != 0 && $transaction_info['blocktime'] < $timestamp)
+                {
+                    //transaction doesn't exist
+                    return [
+                        'exists' => false,
+                        'txid' => ""
+                    ];
+                }
+
+                foreach($transaction_info['tokenTransfers'] as $tokenTransfers)
+                {
+                    if($tokenTransfers['value'] == $amount && $tokenTransfers['symbol'] == "ENJ")
+                    {
+                        return [
+                            'exists' => true,
+                            'txid' => $transaction
+                        ];
+                    }
+                }
+            }
+
+        }
+        catch(\Throwable $e)
+        {
+            throw $e;
+        }
+        
+    }
+
+    //
+    // Check how many confirmations does $txid have (varchar)
+    //
+    public function checkConfirmations($txid)
+    {
+        try{
+            $transaction = $this->getTransaction($txid);
+            return $transaction['confirmations'];
+        }
+        catch (\Throwable $e){
+            throw $e;
+        }
+    }
+
+    //
+    //Get transactions from address
+    //
+    public function getAddressTransactions($address)
+    {
+        try{
+            $addressEndpoint = $this->explorer_url . "address/$address";            
+            $transactions = $this->client->request('GET', $addressEndpoint);
+
+            //convert response into array
+            $transactions_array = (json_decode($transactions->getBody()->getContents(), true))['transactions'];
+            return $transactions_array;
+        }
+        catch (\Throwable $e){
+            throw $e;
+        }
+    }
+
+    //
+    //Get trasnsaction
+    //
+    public function getTransaction($txid)
+    {
+        try{
+            $transactionEndpoint = $this->explorer_url . "tx/$txid"; 
+            $transaction = $this->client->request('GET', $transactionEndpoint);
+
+            //convert response into array
+            $transaction = json_decode($transaction->getBody()->getContents(), true);
+
+            return $transaction;
+        }
+        catch (\Throwable $e){
+            throw $e;
+        }
+    }
+
+}
