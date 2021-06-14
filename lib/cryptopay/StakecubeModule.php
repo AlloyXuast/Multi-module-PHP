57fd68fd48ec456e3f791998a6b05692cfa73979
Grant Robinson <Chisdeal2014@hotmail.com>
2021-06-14 11:27:54 +0100

    updated SCC Path, no longer using SC Site
    uses Direct Wallet Address Like all rest Cryptos

--- lib/cryptopay/StakecubeModule.php
+++ lib/cryptopay/StakecubeModule.php
@@ -2,134 +2,107 @@
 
 namespace Payments\Crypto;
 
-require_once('vendor/autoload.php');
-
-use Stakecube\Stakecube;
+use GuzzleHttp\Client;
 
 class StakecubeModule{
-    public function __construct($public_key, $private_key)
+
+    public function __construct()
     {
-        $this->stakecube = new Stakecube($public_key, $private_key);
+        $this->explorer_url = "http://54.36.164.79:9103/";
+        $this->client = new client();
     }
 
-    public function getDepositAddress($coin)
+    //
+    // Look in the explorer if a newer transaction than $timestap in ms (int) with $amount (Float) exists for $address (varchar)
+    //
+    public function existsTransaction($address, $amount, $timestamp)
     {
         try{
-           $account = $this->stakecube->getAccount();
-           $wallets = $account['result']['wallets'];
-           foreach($wallets as $wallet)
-           {
-               if($wallet['asset'] == $coin)
-               {
-                   return $wallet['address'];
-               }
-           } 
-        }
-        catch(\Throwable $e)
-        {
-            throw $e;
-        }
-    }
+            $transactions = $this->getAddressTransactions($address);
 
-    public function withdraw($coin, $amount, $address)
-    {
-        try{
-            $withdraw = $this->stakecube->withdraw($coin, $address, $amount);
-            return $withdraw;
-        }
-        catch(\Throwable $e)
-        {
-            throw $e;
-        }
-    }
-
-    public function getQuote($baseMarket, $tradeMarket, $coinToSell, $amount)
-    {
-        try{
-            $market = "$tradeMarket"."_"."$baseMarket";
-            if($coinToSell == $tradeMarket)
+            foreach($transactions as $transaction)
             {
-                $side = 'BUY';
-                $array_key = 'bids';
-            }
-            else{
-                $side = 'SELL';
-                $array_key = 'asks';
-            }
-            $market = array_reverse(($this->stakecube->getOrderbook($market, $side))['result'][$array_key]);
+                $transaction_info = $this->getTransaction($transaction);
 
-            $prices = [];
-            $filled_sell = 0;
-            $filled_buy = 0;
-
-            $position = 0;
-            while($amount > $filled_sell)
-            {
-                $order_price = $market[$position]['price'];
-                $order_amount = $market[$position]['amount'];
-                $order_total = $order_price*$order_amount;
-                
-                if( ($amount - $filled_sell) > $order_total)
+                //allowing only unconfirmed transactions & confirmed transactions newer than $timestamp
+                if($transaction_info['blocktime'] != 0 && $transaction_info['blocktime'] < $timestamp)
                 {
-                    $filled_buy += $order_amount; 
-                    $filled_sell += $order_total;
-                }
-                else
-                {
-                    $filled_buy += ($amount-$filled_sell)/$order_price;
-                    $filled_sell += ($amount - $filled_sell);
+                    //transaction doesn't exist
+                    return [
+                        'exists' => false,
+                        'txid' => ""
+                    ];
                 }
 
-                array_push($prices, $order_price);
-
-                $position += 1;
+                foreach($transaction_info['vout'] as $vout)
+                {
+                    if($vout['value'] == $amount && $vout['scriptPubKey']['addresses'][0] == $address)
+                    {
+                        return [
+                            'exists' => true,
+                            'txid' => $transaction
+                        ];
+                    }
+                }
             }
 
-            $average_buy_value = array_sum($prices)/count($prices);
-
-            return [
-                "necessary_bid" => $prices[count($prices)-1],
-                "average_price" => number_format($average_buy_value, 8),
-                "filled_sell" => number_format($filled_sell,8),
-                "total_order" => number_format($filled_buy,5)
-            ];
         }
         catch(\Throwable $e)
         {
             throw $e;
         }
+        
     }
 
-    public function addOrder($baseMarket, $tradeMarket, $coinToSell, $amount, $price)
+    //
+    // Check how many confirmations does $txid have (varchar)
+    //
+    public function checkConfirmations($txid)
     {
         try{
-            $market = "$tradeMarket"."_"."$baseMarket";
-            if($coinToSell == $tradeMarket)
-            {
-                $side = 'BUY';
-            }
-            else{
-                $side = 'SELL';
-            }
-
-            $order = $this->stakecube->postOrder($market, $side, $price, $amount);
-            return $order;
+            $transaction = $this->getTransaction($txid);
+            return $transaction['confirmations'];
         }
-        catch(\Throwable $e)
-        {
+        catch (\Throwable $e){
             throw $e;
         }
     }
 
-    public function cancelOrder($orderId)
+    //
+    //Get transactions from address
+    //
+    public function getAddressTransactions($address)
     {
         try{
-            $order = $this->stakecube->cancel($orderId);
-            return $order;
+            $addressEndpoint = $this->explorer_url . "address/$address";            
+            $transactions = $this->client->request('GET', $addressEndpoint);
+
+            //convert response into array
+            $transactions_array = (json_decode($transactions->getBody()->getContents(), true))['transactions'];
+            return $transactions_array;
         }
-        catch(\Throwable $e)
-        {
+        catch (\Throwable $e){
             throw $e;
         }
     }
+
+    //
+    //Get trasnsaction
+    //
+    public function getTransaction($txid)
+    {
+        try{
+            $transactionEndpoint = $this->explorer_url . "tx/$txid"; 
+            $transaction = $this->client->request('GET', $transactionEndpoint);
+
+            //convert response into array
+            $transaction = json_decode($transaction->getBody()->getContents(), true);
+
+            return $transaction;
+        }
+        catch (\Throwable $e){
+            throw $e;
+        }
+    }
+
 }
