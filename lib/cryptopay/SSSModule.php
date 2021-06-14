ab4e5c685eebd33e672b1f99117b59965920a20b
Grant Robinson <Chisdeal2014@hotmail.com>
2021-06-14 11:36:45 +0100

    Create SSSModule.php

---
+++ lib/cryptopay/SSSModule.php
@@ -0,0 +1,108 @@
+<?php 
+
+namespace Payments\Crypto;
+
+use GuzzleHttp\Client;
+
+class SSSModule{
+    
+    public function __construct()
+    {
+        $this->explorer_url = "http://54.36.164.79:9118/api/v1/";
+        $this->client = new client();
+    }
+
+    //
+    // Look in the explorer if a newer transaction than $timestap in ms (int) with $amount (Float) exists for $address (varchar)
+    //
+    public function existsTransaction($address, $amount, $timestamp)
+    {
+        try{
+            $transactions = $this->getAddressTransactions($address);
+
+            foreach($transactions as $transaction)
+            {
+                $transaction_info = $this->getTransaction($transaction);
+
+                //allowing only unconfirmed transactions & confirmed transactions newer than $timestamp
+                if($transaction_info['blocktime'] != 0 && $transaction_info['blocktime'] < $timestamp)
+                {
+                    //transaction doesn't exist
+                    return [
+                        'exists' => false,
+                        'txid' => ""
+                    ];
+                }
+
+                foreach($transaction_info['vout'] as $vout)
+                {
+                    if($vout['value'] == $amount && $vout['scriptPubKey']['addresses'][0] == $address)
+                    {
+                        return [
+                            'exists' => true,
+                            'txid' => $transaction
+                        ];
+                    }
+                }
+            }
+
+        }
+        catch(\Throwable $e)
+        {
+            throw $e;
+        }
+        
+    }
+
+    //
+    // Check how many confirmations does $txid have (varchar)
+    //
+    public function checkConfirmations($txid)
+    {
+        try{
+            $transaction = $this->getTransaction($txid);
+            return $transaction['confirmations'];
+        }
+        catch (\Throwable $e){
+            throw $e;
+        }
+    }
+
+    //
+    //Get transactions from address
+    //
+    public function getAddressTransactions($address)
+    {
+        try{
+            $addressEndpoint = $this->explorer_url . "address/$address";            
+            $transactions = $this->client->request('GET', $addressEndpoint);
+
+            //convert response into array
+            $transactions_array = (json_decode($transactions->getBody()->getContents(), true))['transactions'];
+            return $transactions_array;
+        }
+        catch (\Throwable $e){
+            throw $e;
+        }
+    }
+
+    //
+    //Get trasnsaction
+    //
+    public function getTransaction($txid)
+    {
+        try{
+            $transactionEndpoint = $this->explorer_url . "tx/$txid"; 
+            $transaction = $this->client->request('GET', $transactionEndpoint);
+
+            //convert response into array
+            $transaction = json_decode($transaction->getBody()->getContents(), true);
+
+            return $transaction;
+        }
+        catch (\Throwable $e){
+            throw $e;
+        }
+    }
+
+}
