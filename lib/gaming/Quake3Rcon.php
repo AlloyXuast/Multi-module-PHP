cc32ceb8057909a6fa5c9b3ce88aa4078a6346f6
Grant Robinson <Chisdeal2014@hotmail.com>
2021-07-01 14:41:51 +0100

    Update and rename phpQuake3.php to Quake3Rcon.php

--- lib/gaming/Quake3Rcon.php
+++ lib/gaming/Quake3Rcon.php
@@ -19,7 +19,9 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
-class q3query {
+namespace Payments\Gaming;
+
+class Quake3Rcon {
 
         private $rconpassword;
         private $fp;
