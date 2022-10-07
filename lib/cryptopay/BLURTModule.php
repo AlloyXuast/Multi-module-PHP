094d7d6da15e6648b32fe3e29272c15e31a3a6ce
Grant Robinson <Chisdeal2014@hotmail.com>
2022-10-07 19:58:54 +0100

    Update BLURTModule.php

--- lib/cryptopay/BLURTModule.php
+++ lib/cryptopay/BLURTModule.php
@@ -8,7 +8,7 @@ class BLURTModule{
     
     public function __construct()
     {
-        $this->explorer_url = "https://api.chisdealhd.co.uk/v1/cryptoproxyexplorer/blurt/";
+        $this->explorer_url = "https://api.chisdealhd.co.uk/v2/crypto/proxyexplorer/blurt/";
         $this->client = new client();
     }
 
