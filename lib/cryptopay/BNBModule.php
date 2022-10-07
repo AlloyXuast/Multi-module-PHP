fc67bc2d5c3c8f52abf9506e3e60bb14b7188aa0
Grant Robinson <Chisdeal2014@hotmail.com>
2022-10-07 19:59:09 +0100

    Update BNBModule.php

--- lib/cryptopay/BNBModule.php
+++ lib/cryptopay/BNBModule.php
@@ -8,7 +8,7 @@ class BNBModule{
     
     public function __construct()
     {
-        $this->explorer_url = "https://api.chisdealhd.co.uk/v1/cryptoproxyexplorer/bnb/";
+        $this->explorer_url = "https://api.chisdealhd.co.uk/v2/crypto/proxyexplorer/bnb/";
         $this->client = new client();
     }
 
