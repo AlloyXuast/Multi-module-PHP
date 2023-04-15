3f83f53aa2470298010920abad0d36a8009726a6
NekoSuneVR <Chisdeal2014@hotmail.com>
2023-04-16 00:55:45 +0100

    Update HIVEModule.php

--- lib/cryptopay/HIVEModule.php
+++ lib/cryptopay/HIVEModule.php
@@ -8,7 +8,7 @@ class HIVEModule{
     
     public function __construct()
     {
-        $this->explorer_url = "https://api.chisdealhd.co.uk/v2/crypto/proxyexplorer/hive/";
+        $this->explorer_url = "https://api.nekosunevr.co.uk/v3/payments/api/hive/";
         $this->client = new client();
     }
 
