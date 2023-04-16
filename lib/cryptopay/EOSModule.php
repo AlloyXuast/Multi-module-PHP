b62ca429c29fd342e3fbf47e18f855f2ece7cb6c
NekoSuneVR <Chisdeal2014@hotmail.com>
2023-04-16 15:11:22 +0100

    Update and rename EOSModule.php.disabled to EOSModule.php

--- lib/cryptopay/EOSModule.php
+++ lib/cryptopay/EOSModule.php
@@ -8,7 +8,7 @@ class EOSModule{
     
     public function __construct()
     {
-        $this->explorer_url = "https://api.chisdealhd.co.uk/v2/crypto/proxyexplorer/eos/";
+        $this->explorer_url = "https://api.nekosunevr.co.uk/v3/payments/api/eos/";
         $this->client = new client();
     }
 
